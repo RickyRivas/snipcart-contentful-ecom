ce496d4bc532181d3648d3f1cf09854bba4e4324
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-12-05 20:26:12 -0600

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -47,8 +47,8 @@ class Products {
 class Ui {
     displayProducts(products) {
         let result = '';
-        const endpoint = 'https://rwde2.netlify.app/'
-        const page = 'index.html'
+        const endpoint = 'https://rwde2.netlify.app'
+        const folder = 'scripts'
         const fileName = 'products.json'
         products.forEach(product => {
             result += `
@@ -65,7 +65,7 @@ class Ui {
                 class='bag-btn snipcart-add-item' 
                 data-item-id="${product.id}" 
                 data-item-price="${product.price}"
-                data-item-url="${endpoint}/${page}/${fileName}"
+                data-item-url="${endpoint}/${folder}/${fileName}"
                 data-item-description="${product.description}"
                 data-item-image="${product.image}"
                 data-item-name='${product.title}'
